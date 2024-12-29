b61f8108f2dba174c4ffb82367671a50def1a48f
syjoy1993 <syjoy1993@gmail.com>
2024-12-29 19:02:28 +0900

    docker compose with Actions init

---
+++ src/main/java/actions/DockerComposeWithApplication.java
@@ -0,0 +1,12 @@
+package actions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerComposeWithApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DockerComposeWithApplication.class, args);
+    }
+
+}
